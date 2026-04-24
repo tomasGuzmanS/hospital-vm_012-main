9da7d37194251e416c7c8674785a29d1f1e17a75
Tomas <to.guzmang@duocuc.cl>
2026-04-23 22:12:26 -0400

    commit "delete"

--- src/main/java/com/hospital_vm_cl/hospital_vm/controller/PacienteController.java
+++ src/main/java/com/hospital_vm_cl/hospital_vm/controller/PacienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,4 +74,14 @@ public class PacienteController {
         }
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        try{
+            pacienteService.findById(id);
+            pacienteService.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch(Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
